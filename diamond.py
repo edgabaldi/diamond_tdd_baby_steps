4f39bba8b2dad722ac1c80368801a928e717deaa
Edgar Gabaldi <edgabaldi@gmail.com>
2016-04-04 21:56:43 -0300

    Step 1: Retornando um stub

--- diamond.py
+++ diamond.py
@@ -1,9 +1,12 @@
 import unittest
 
+def interno(letra):
+    return 'a'
+
 class DiamondTestCase(unittest.TestCase):
     
-    def test_something(self):
-        assert 1==1
+    def test_interno_a(self):
+        assert interno('a') == 'a'
 
 if __name__ == '__main__':
     unittest.main()
