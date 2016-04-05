7600579ae33099e6b79b6e518f258c14c55964bf
Edgar Gabaldi <edgabaldi@gmail.com>
2016-04-04 23:11:59 -0300

    Step 37: Fazendo espaco_externo(letra='b', posicao='a') retornar stub

--- diamond.py
+++ diamond.py
@@ -12,6 +12,8 @@ def espaco_interno(letra):
     return qtde * ' '
 
 def espaco_externo(letra, posicao):
+    if letra == 'b' and posicao == 'a':
+        return ' '
     return ''
 
 def interno(letra):
