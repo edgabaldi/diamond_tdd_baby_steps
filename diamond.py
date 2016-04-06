46560eb2bb4ebcb454682bf171bedb3f4ba6bccc
Edgar Gabaldi <edgabaldi@gmail.com>
2016-04-05 21:16:24 -0300

    Step 70: substituindo constantes em diamond('b') para chamadas para linha()

--- diamond.py
+++ diamond.py
@@ -35,7 +35,10 @@ def linha(letra, posicao):
 
 def diamond(letra):
     if letra == 'b':
-        return '\n'.join([linha(letra='b', posicao='a'), 'b b', ' a'])
+        return '\n'.join([
+            linha(letra='b', posicao='a'),
+            linha(letra='b', posicao='b'),
+            linha(letra='b', posicao='a')])
     if letra == 'c':
         return "  a\n b b\nc   c\n b b\n  a"
     return letra
